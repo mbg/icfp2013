706ac00263b8dd5a30c63edca8677ea2d2151ca3
David McGillicuddy <dmcgill9071@gmail.com>
2013-08-09 12:58:43 +0100

    added 'eval', a high level interface to Eval

--- Eval.hs
+++ Eval.hs
@@ -14,21 +14,28 @@ import Data.Aeson
 import Data.ByteString.Lazy.Char8 (pack, unpack)
 import Data.Maybe (fromJust)
 import Data.Word (Word64)
+import Debug.Trace
 import Network.HTTP
 import Text.Printf
 
 import AST
 import Config
-import PrettyPrint 
+import PrettyPrint
 
-evalProgram :: Program -> [Word64] -> IO (Maybe [Word64])
-evalProgram ast args = evalRemotely request >>= maybe httpErr (\(ERes status mOuts mMsg) ->
-    if status == "ok"
-        then return (map read <$> mOuts)
-        else (print mMsg >> return Nothing))
+eval :: Either String Program -> [Word64] -> IO (Maybe [Word64])
+eval idOrProg args = (>>= interpretResponse) <$> evalRemotely request
     where
-    request = EReq Nothing (Just (ppProgram ast "")) (map (printf "0x%016X") args)
-    httpErr = putStr "HTTP Request or JSON parsing failed in evalProgram" >> return Nothing
+    (id, prog) = either
+        (\id'   -> (Just id', Nothing                  ))
+        (\prog' -> (Nothing , Just $ ppProgram prog' ""))
+        idOrProg
+    request = EReq id prog (map (printf "0x%016X") args)
+
+interpretResponse :: EvalResponse -> Maybe [Word64]
+interpretResponse (ERes status mOuts mMsg) =
+    if status == "ok"
+        then map read <$> mOuts
+        else traceShow mMsg Nothing
 
 data EvalRequest = EReq {
     ereqID      :: Maybe String,
@@ -46,20 +53,20 @@ data EvalResponse = ERes {
 }
 
 instance FromJSON EvalResponse where
-    parseJSON (Object v) = ERes <$> 
+    parseJSON (Object v) = ERes <$>
                             v .: "status"   <*>
                             v .:? "outputs" <*>
                             v .:? "message"
 
 evalURI :: String
 evalURI = "http://icfpc2013.cloudapp.net/eval?auth=" ++ apiKey ++ "vpsH1H"
-                            
-evalRemotely :: EvalRequest -> IO (Maybe EvalResponse) 
+
+evalRemotely :: EvalRequest -> IO (Maybe EvalResponse)
 evalRemotely req = do
     rsp <- simpleHTTP $ postRequestWithBody evalURI "text/plain" (unpack (encode req))
     bdy <- pack <$> getResponseBody rsp
     return (decode bdy)
-    
+
 evalString :: String -> IO ()
 evalString str = do
     mer <- evalRemotely $ EReq Nothing (Just str) defaultArgs
@@ -67,7 +74,7 @@ evalString str = do
         Nothing   -> putStrLn "Unable to evaluate string."
         (Just er) -> case (eresStatus er) of
             "ok"    -> mapM_ putStrLn (fromJust $ eresOuts er)
-            "error" -> putStrLn $ "error: " ++ (fromJust $ eresMsg er) 
+            "error" -> putStrLn $ "error: " ++ (fromJust $ eresMsg er)
 
 defaultArgs :: [String]
 defaultArgs = ["0x0000000000000000", "0x0000000000000001", "0x0000000000000002", "0xFFFFFFFFFFFFFFFF"]
